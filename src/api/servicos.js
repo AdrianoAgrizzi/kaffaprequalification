4aaf45f9ac0f6ade3b5d4513165659cccb8496d4
Adriano Agrizzi <adriano.agrizzi@gmail.com>
2020-07-23 11:14:28 -0300

    Update

--- src/api/servicos.js
+++ src/api/servicos.js
@@ -7,33 +7,39 @@ const Servicos = function(servicos) {
 
 // Matriz que armazena os retângulos
 var rectangles = [];
+var area_rectangles = [];
 
 // 3) Test if two rectangles intersect
-Servicos.addrectangle = (rect, retorno) => {
+Servicos.addrectangle = (rect, area, retorno) => {
   // serviço para adicionar os retângulos
-  rect = rect.trim();
+  rect = rect.trim().split('&')[0];
   rect = rect.replace("(","").replace(")","");
 
   // Pega automaticamete a letra no queystring e guarda como nome do retângulo
+  console.log("rect: "+rect);
   let key    = String(rect.split('=')[0]).toUpperCase();
   let values = rect.split('=')[1];
-  let r = rectangles.indexOf(key);
+  let rectAux = [];
+
+  area = area == "true";
+  area ? rectAux = area_rectangles : rectAux = rectangles;
+  let r = rectAux.indexOf(key);
 
   // Adiciona 
   if ( r == -1 ) {
-    rectangles.push(key);
-    rectangles.push(values.split(';')[0]);
-    rectangles.push(values.split(';')[1]);
+    rectAux.push(key);
+    rectAux.push(values.split(';')[0]);
+    rectAux.push(values.split(';')[1]);
   } 
   // Altera
   else{
-    rectangles[r+1] = values.split(';')[0];
-    rectangles[r+2] = values.split(';')[1];
-}
+    rectAux[r+1] = values.split(';')[0];
+    rectAux[r+2] = values.split(';')[1];
+  }
 
   retorno(0);
 };
-Servicos.intersects = (rect, retorno) => {
+Servicos.intersects = (rect, area, retorno) => {
   // Realiza o cálculo da interseção dos retângulos
   if (rect == undefined || rect.trim() == ""){
     retorno(false);
@@ -43,25 +49,31 @@ Servicos.intersects = (rect, retorno) => {
   let rect1 = String(rect.split(',')[0]).toUpperCase();
   let rect2 = String(rect.split(',')[1]).toUpperCase();
 
+
+  let rectAux = [];
+  area = area == "true";
+  area ? rectAux = area_rectangles.slice() : rectAux = rectangles.slice();
+
+  console.log(rectAux);
   // Verifica se os retângulos foram adicionados
-  let r1 = rectangles.indexOf(rect1);
+  let r1 = rectAux.indexOf(rect1);
   if ( r1 == -1 ) {
     retorno(`Retângulo ${rect1} não encontrado!`);
     return;
   }
-  let r2 = rectangles.indexOf(rect2);
+  let r2 = rectAux.indexOf(rect2);
   if ( r2 == -1 ) {
     retorno(`Retângulo ${rect2} não encontrado!`);
     return;
   }
 
   // Pega os pontos do Retangulo 1
-  let rect1P1 = rectangles[r1+1].split(',');
-  let rect1P2 = rectangles[r1+2].split(',');
+  let rect1P1 = rectAux[r1+1].split(',');
+  let rect1P2 = rectAux[r1+2].split(',');
 
   // Pega os pontos do Retangulo 2
-  let rect2P1 = rectangles[r2+1].split(',');
-  let rect2P2 = rectangles[r2+2].split(',');
+  let rect2P1 = rectAux[r2+1].split(',');
+  let rect2P2 = rectAux[r2+2].split(',');
 
   // Arrays para armazenas a área de cada retângulo
   let rect1X = [];
@@ -103,8 +115,33 @@ Servicos.intersects = (rect, retorno) => {
     }
   }
 
+
   if ( yesIntersectsX && yesIntersectsY ) {
-    retorno(true);
+
+    console.log(area);
+    if (area){
+      console.log("area");
+      // Realiza o cálculo da área Z
+      let contAreaX = 0;
+      for (i in rect1X){
+        if ( rect2X.indexOf(rect1X[i]) > -1 ){
+          contAreaX++;
+        }
+      }
+      // Realiza o cálculo da área Y
+      let contAreaY = 0;
+      for (i in rect1Y){
+        if ( rect2Y.indexOf(rect1Y[i]) > -1 ){
+          contAreaY++;
+        }
+      }
+      
+      retorno(contAreaX * contAreaY);
+    }
+    else{
+      console.log("interseção");
+      retorno(true);
+    }
   }
   else{
     retorno(false);
--- src/routes.js
+++ src/routes.js
@@ -5,15 +5,16 @@ const querystring = require('querystring');
 module.exports = function(server) {
         
     server.get('/api/addrectangle', (req, res) => {
-        Servicos.addrectangle(decodeURIComponent(querystring.stringify(req.query)), (ret) => {
+        Servicos.addrectangle(decodeURIComponent(querystring.stringify(req.query)), req.query.area, (ret) => {
             res.send(String(ret));
         });
     })    
     server.get('/api/intersects', (req, res) => {
-        Servicos.intersects(req.query.i, (ret) => {
+        Servicos.intersects(req.query.i, req.query.area, (ret) => {
             res.send(String(ret));
         });
     })    
+    
 
     server.get('/', (req, res) => {
         fs.readFile('src/index.html', (err, data) => {
